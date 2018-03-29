fbc1629275f7f471121d074b8b51d0375f4864b2
Anna Johansson <anna@hoxton.se>
2018-03-29 09:02:09 +0200

    Initial commit

---
+++ ubuntu/xenial64/setup/setup-virtualhost.sh
@@ -0,0 +1,168 @@
+#!/bin/bash
+
+function say {
+    printf "\n--------------------------------------------------------\n"
+    printf "$1"
+	printf "\n--------------------------------------------------------\n"
+}
+
+### Set default parameters
+action=create
+domain=$1
+rootDir="$1/www"
+owner=$(who am i | awk '{print $1}')
+apacheUser=$(ps -ef | egrep '(httpd|apache2|apache)' | grep -v root | head -n1 | awk '{print $1}')
+email='webmaster@vagrant'
+sitesEnabled='/etc/apache2/sites-enabled/'
+sitesAvailable='/etc/apache2/sites-available/'
+userDir='/var/www/'
+sitesAvailabledomain=$sitesAvailable$domain.conf
+
+if [ "$action" != 'create' ] && [ "$action" != 'delete' ]
+	then
+		echo $"You need to prompt for action (create or delete) -- Lower-case only"
+		exit 1;
+fi
+
+while [ "$domain" == "" ]
+do
+	echo -e $"Please provide domain. e.g.dev,staging"
+	read domain
+done
+
+if [ "$rootDir" == "" ]; then
+	rootDir=${domain//./}
+fi
+
+### if root dir starts with '/', don't use /var/www as default starting point
+if [[ "$rootDir" =~ ^/ ]]; then
+	userDir=''
+fi
+
+rootDir=$userDir$rootDir
+
+if [ "$action" == 'create' ]
+	then
+		### check if domain already exists
+		if [ -e $sitesAvailabledomain ]; then
+			echo -e $"$sitesAvailabledomain already exists.\n"
+			exit;
+		fi
+
+		### check if directory exists or not
+		if ! [ -d $rootDir ]; then
+			mkdir -p $rootDir
+			chmod -R 775 $rootDir
+		fi
+
+		### create virtual host rules file
+		if ! echo "
+		<VirtualHost *:80>
+			ServerAdmin $email
+			ServerName $domain
+			ServerAlias $domain
+			DocumentRoot $rootDir
+			<Directory />
+				AllowOverride All
+			</Directory>
+			<Directory $rootDir>
+				Options Indexes FollowSymLinks MultiViews
+				AllowOverride all
+				Require all granted
+			</Directory>
+			ErrorLog /var/log/apache2/$domain-error.log
+			LogLevel error
+			CustomLog /var/log/apache2/$domain-access.log combined
+		</VirtualHost>" > $sitesAvailabledomain
+		then
+			echo -e $"There was an ERROR creating $domain file"
+			exit;
+		fi
+
+		### Add domain in /etc/hosts
+		if ! echo "127.0.0.1	$domain" >> /etc/hosts
+		then
+			echo $"ERROR: Not able to write to /etc/hosts"
+			exit;
+		fi
+
+		### Add domain in /mnt/c/Windows/System32/drivers/etc/hosts (Windows Subsytem for Linux)
+		if [ -e /mnt/c/Windows/System32/drivers/etc/hosts ]
+		then
+			if ! echo -e "\r127.0.0.1       $domain" >> /mnt/c/Windows/System32/drivers/etc/hosts
+			then
+				echo $"ERROR: Not able to write in /mnt/c/Windows/System32/drivers/etc/hosts (Hint: Try running Bash as administrator)"
+			else
+				echo -e $"Host added to /mnt/c/Windows/System32/drivers/etc/hosts file \n"
+			fi
+		fi
+
+		if [ "$owner" == "" ]; then
+			iam=$(whoami)
+			if [ "$iam" == "root" ]; then
+				chown -R $apacheUser:$apacheUser $rootDir
+			else
+				chown -R $iam:$iam $rootDir
+			fi
+		else
+			chown -R $owner:$owner $rootDir
+		fi
+
+		### enable website
+		a2ensite $domain  >/dev/null 2>&1
+
+		### restart Apache
+		/etc/init.d/apache2 reload  >/dev/null 2>&1
+
+		### show the finished message
+		echo -e $"New host added: http://$domain ($rootDir)"
+		exit;
+	else
+		### check whether domain already exists
+		if ! [ -e $sitesAvailabledomain ]; then
+			echo -e $"This domain does not exist."
+			exit;
+		else
+			### Delete domain in /etc/hosts
+			newhost=${domain//./\\.}
+			sed -i "/$newhost/d" /etc/hosts
+
+			### Delete domain in /mnt/c/Windows/System32/drivers/etc/hosts (Windows Subsytem for Linux)
+			if [ -e /mnt/c/Windows/System32/drivers/etc/hosts ]
+			then
+				newhost=${domain//./\\.}
+				sed -i "/$newhost/d" /mnt/c/Windows/System32/drivers/etc/hosts
+			fi
+
+			### disable website
+			a2dissite $domain
+
+			### restart Apache
+			/etc/init.d/apache2 reload
+
+			### Delete virtual host rules files
+			rm $sitesAvailabledomain
+		fi
+
+		### check if directory exists or not
+		if [ -d $rootDir ]; then
+			echo -e $"Delete host root directory ? (y/n)"
+			read deldir
+
+			if [ "$deldir" == 'y' -o "$deldir" == 'Y' ]; then
+				### Delete the directory
+				rm -rf $rootDir
+				echo -e $"Directory deleted"
+			else
+				echo -e $"Host directory conserved"
+			fi
+		else
+			echo -e $"Host directory not found. Ignored"
+		fi
+
+		### show the finished message
+		echo -e $"Complete!\nYou just removed Virtual Host $domain"
+		exit 0;
+fi
+
+say "Virtualhosts setup done."
